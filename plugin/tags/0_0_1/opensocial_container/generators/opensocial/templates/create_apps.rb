2c272138a030d62156d52b77972e30e6d89f6de1
rgarver <rgarver@8f5e3bfa-6c41-48a6-93b2-11dd833e12a9>
2007-11-27 05:28:54 +0000

    Tag v0.0.1

---
+++ plugin/tags/0_0_1/opensocial_container/generators/opensocial/templates/create_apps.rb
@@ -0,0 +1,32 @@
+class CreateApps < ActiveRecord::Migration
+  def self.up
+    create_table :apps do |t|
+      t.string :source_url
+      t.string :title
+      t.text :description
+      t.string :directory_title
+      t.string :title_url
+      t.string :author
+      t.string :author_email
+      t.string :author_affiliation
+      t.string :author_location
+      t.string :screenshot
+      t.string :thumbnail
+      t.integer :height
+      t.integer :width
+      t.boolean :scaling
+      t.boolean :scrolling
+      t.boolean :singleton
+      t.string :author_photo
+      t.text :author_aboutme
+      t.string :author_link
+      t.text :author_quote
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :apps
+  end
+end
