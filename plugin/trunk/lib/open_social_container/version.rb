cabc83c3ecfcf1eaa2671936254b94ce8dc2c3d8
rgarver <rgarver@8f5e3bfa-6c41-48a6-93b2-11dd833e12a9>
2007-12-17 17:54:41 +0000

    Update version 1.0 woohoo

--- plugin/trunk/lib/open_social_container/version.rb
+++ plugin/trunk/lib/open_social_container/version.rb
@@ -1,8 +1,8 @@
 module OpenSocialContainer
   module VERSION #:nodoc:
-    MAJOR = 0
-    MINOR = 3
-    TINY  = 1
+    MAJOR = 1
+    MINOR = 0
+    TINY  = 0
 
     STRING = [MAJOR, MINOR, TINY].join('.')
   end
